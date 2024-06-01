8d4162f3ec2a82acb7343150522b12976af9c1ec
chrisOburu <chrizoburu65@gmail.com>
2024-06-01 14:33:13 +0300

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",2,"M","grey","yes",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",2,"F","grey","yes",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",2,"M","grey","yes",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",2,"M","grey","yes",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",2,"F","grey","yes",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",2,"M","grey","yes",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",2,"M","grey","yes",TRUE);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,2,"F","grey","yes",TRUE);
